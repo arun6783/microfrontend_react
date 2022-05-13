722ce1c4510b7da86f4e9c8690a057fc3e70509c
Arunkumar Narayanaswamy <arunkmr1983@gmail.com>
2022-05-13 10:56:05 +0100

    adding prod config and also creating new build yml

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const commonConfig = require('./webpack.common')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const devConfig = {
@@ -17,9 +16,6 @@ const devConfig = {
       exposes: {
         './MarketingApp': './src/bootstrap',
       },
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
     }),
   ],
 }
