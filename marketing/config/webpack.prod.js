39ef4179586c28487ea7f76e74146548fe52a298
Arunkumar Narayanaswamy <arunkmr1983@gmail.com>
2022-05-16 21:57:04 +0100

    adding auth project and wiring up all components

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -1,7 +1,8 @@
 const { merge } = require('webpack-merge')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
-const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
+const commonConfig = require('./webpack.common')
+
 const prodConfig = {
   mode: 'production',
   output: {
